95e1e9530f8233986fd3bcb5843421975e41f275
vasmarm <sharmavashisth@gmail.com>
2017-11-01 21:23:49 -0500

    Random Animal Picture Display

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,6 +10,7 @@ var animal = ""; var aniType = ""; var zipCode = ""; var breed = ""; var age = "
 
 $(document).ready(function() {
 
+
     //set the Adv Inputs to be slid up
     function startHidden() {
         $("#hider").hide();
@@ -227,6 +228,43 @@ $(document).ready(function() {
             //make a carosel, based on animal type? or just a randome pet carousel?
         }
 });
+
+// Random Animal Generator API
+
+
+function randomPet(){
+
+    var randomAnimalArray = ["dog","cat","horse","bird","barnyard"];
+    randomSearch = randomAnimalArray[Math.floor(Math.random() * (5 - 0)) + 0]; 
+    console.log(randomSearch);
+    var randomZipArray = [61704,53072,11724,11725,11726,11727,11729,11730,11731,11732,
+                          11733,11735,11737,11738,11739,11740,11741,11742,11743,11746,
+                          11747,11749,11751,11752,11753,11755,11756,11757,11758,11760,
+                          11762,11763,11764,11765,11766,11767,11768,11769,11770,11771,
+                          11772,11773,11775,11776,11777,33428,33429,33430,33431,33432,
+                          33433,33434,33435,33436,33437,33576,33578,33579,33583,33584,
+                          33585,33731,33732,33733,33734,33736,33737,33738,35013,35014,
+                          35015,35016,35019,35020,35021,35022,35023,35212,35213,35214,
+                          35215,35216,35217,35218,35219,35220,35221,40588,40591,40598,
+                          40601,40602,40603,40604,40618,40619,41074,41075,41076,41080];
+    var randomZip = randomZipArray[Math.floor(Math.random() * (100 - 0)) + 0];
+    console.log(randomZip);
+  
+
+    var url = "http://api.petfinder.com/pet.find?format=json&key=0dbe85d873e32df55a5a7be564ae63a6&callback=?&animal="+randomSearch+"&location="+randomZip+"&count=1";
+    $.ajax({
+    url: url,
+    dataType: 'jsonp',
+    method: 'GET',
+    }).done(function(result) {
+        console.log(result);
+
+        $(".randomImage").append("<img src="+result.petfinder.pets.pet.media.photos.photo[2].$t+"/ >");
+
+});
+}
+
+
 // not in the document.ready!
 //petfinder API function
 function callPets(animal, location){
@@ -236,6 +274,7 @@ function callPets(animal, location){
     dataType: 'jsonp',
     method: 'GET',
     }).done(function(result) {
+       // console.log(result);    
 
         for(var i = 0; i < result.petfinder.pets.pet.length; i++){
             $("table").append("<tr class='showData' id='"+i+"'><td colspan=2><img src='"+result.petfinder.pets.pet[i].media.photos.photo[2].$t+"'/ ></td><td>"+result.petfinder.pets.pet[i].name.$t+"</td></tr><tr class='hideData' data='hidden' id='a"+i+"'><td>"+result.petfinder.pets.pet[i].description.$t+"</td></tr>");
@@ -251,7 +290,8 @@ function callPets(animal, location){
     });
 };
 
-callPets("dog", 60640);
+callPets("dog", 61704);
+randomPet();
 
 $(document).on("click", ".showData", function(){
     if($("#a"+$(this).attr("id")).attr('data') === "hidden"){
